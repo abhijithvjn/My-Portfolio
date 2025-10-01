60bcbd164ee1cbaa512cf21b8149408dfd4ddb10
abhijiith-wac <abhijith.v@webandcrafts.in>
2025-10-02 01:57:00 +0530

    fixes

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -1,8 +1,7 @@
-
 import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,8 +10,8 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-    historyApiFallback: true
-  }
+  },
+  server: {
+    historyApiFallback: true,
   },
 })
